7b04cfc9175761c5382e825a767752cdfb3dec3b
George <37842292+Gheiney23@users.noreply.github.com>
2023-03-10 08:28:57 -0500

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,15 @@
+
+
+sku_list = [
+'1030-5603/15', 
+'1030-5603/26', 
+'106/04', 
+'106/10', 
+'106/15S', 
+'106/56', 
+'108/15'
+]
+
+link_dict = {'Install': ['-installation-sheet.pdf', '_installation_sheet.pdf', 'install.pdf'], 'Specs': ['specification-sheet.pdf', 'specification_sheet.pdf', 'spec.pdf']}
+
+print(link_dict['Install'])
